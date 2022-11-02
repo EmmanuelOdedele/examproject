7eb6c25c8817084312bb5bbfb040f4f700626327
EmmanuelOdedele <emmaodedele@gmail.com>
2022-11-02 22:58:07 +0000

    API fetch of github repos implemented

--- src/components/GithubRepos.js
+++ src/components/GithubRepos.js
@@ -1,20 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navigation from "./Navigation";
-
+import axios from "axios";
 
 function GithubRepos() {
-  
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const res = await fetch("https://api.github.com/users/emmanuel")
-  //   }
-  // })
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setHasError(false);
+      try {
+        const results = await axios(
+          "https://api.github.com/users/EmmanuelOdedele/repos"
+        );
+        setData(results.data);
+      } catch (error) {
+        setHasError(true);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  console.log(data);
 
 
   return (
     <section>
-        <Navigation/>
+      <Navigation />
       <h1>My GitHub Repos</h1>
+      {hasError && (
+        <p>
+          Unable load repositories.
+          <br />
+          Error might be due to poor internet connection.
+        </p>
+      )}
+      {isLoading ? (
+        <p>Loading repositories</p>
+      ) : (
+        <ul>
+          {data.map((repo) => (
+            <li key={repo.id}>
+              {repo.name}
+              <a href={repo.html_url} rel="noreferrer" target="_blank">Code</a>
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 }
